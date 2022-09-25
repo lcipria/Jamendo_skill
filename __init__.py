c39f5cfea78c99e3a17bdebd91870cd993b4825d
lcipria <lcipria@comune.genova.it>
2022-09-25 14:49:16 +0200

    improvement

--- __init__.py
+++ __init__.py
@@ -155,23 +155,30 @@ class Jamendo_skill(MycroftSkill):
     @intent_handler('search_artists_tags.intent')
     def search_artists_tags(self, message):
         artist_name = message.data.get('artist_name')
-        tags = [ tags for artist in self.__get_artists_musicinfo__({'name': artist_name}).get('results') for tags in artist.get('musicinfo').get('tags') ]
-        if tags:
-            self.speak_dialog('search_artists_tags', {
-                'artist_name': artist_name,
-                'list': join_list(tags, 'and')
-            })
+        artists = self.__get_artists_musicinfo__({'namesearch': artist_name}).get('results')
+        if artists:
+            selected_artist = self.ask_selection([ x.get('name') for x in artists ])
+            if selected_artist:
+                selected_artist = [ x for x in artists if x.get('name') == selected_artist ][0]
+                selected_artist_tags = list(set(selected_artist.get('musicinfo').get('tags')))
+                if selected_artist_tags:
+                    self.speak_dialog('search_artists_tags', {
+                        'artist_name': selected_artist.get('name'),
+                        'list': join_list(selected_artist_tags, 'and')
+                    })
+                else:
+                    self.speak_dialog('no_tags')
+            else:
+                self.speak_dialog('no_artists')
         else:
-            return false
+            self.speak_dialog('no_artists')
 
     @intent_handler('search_albums_tags.intent')
     def search_albums_tags(self, message):
         album_name = message.data.get('album_name')
         artist_name = message.data.get('artist_name')
         albums = self.__get_albums_musicinfo__({'artist_name': artist_name, 'name': album_name}).get('results') # namesearch for fuzzy match?
-        if len(albums) == 0:
-            self.speak_dialog('no_albums')
-        else:
+        if albums:
             selected_album = self.ask_selection([ self.__describe_album__(x) for x in albums ], min_conf=0)
             selected_album = [ x for x in albums if self.__describe_album__(x) == selected_album ][0]
             self.speak_dialog('search_albums_tags', {
@@ -181,6 +188,8 @@ class Jamendo_skill(MycroftSkill):
                     list(set(selected_album.get("musicinfo").get("tags"))), "and"
                 )
             })
+        else:
+            self.speak_dialog('no_albums')
 
 def create_skill():
     return Jamendo_skill()
